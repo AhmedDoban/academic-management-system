08428ae7b7a669d9e30a3a46d436731e3e211a1e
ahmeddoban <ahmedzoro323@gmail.com>
2022-11-23 04:15:21 +0200

    update style

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import Student from "./assets/js/Student/student";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Student />
     </BrowserRouter>
   </React.StrictMode>
 );
