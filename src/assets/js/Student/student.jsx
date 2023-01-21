16cc3d5286d39547e3d49f58e14e2b00981ac056
ahmeddoban <ahmedzoro323@gmail.com>
2023-01-21 15:08:53 +0200

    update bug

--- src/assets/js/Student/student.jsx
+++ src/assets/js/Student/student.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import SideBar from "../components/SideBar";
 import Head from "../Header/Head";
 import { Route, Routes, Navigate } from "react-router-dom";
-import StudentHomePage from "./StudentHomePage";
-import StudentSettingPage from "./StudentSettingPage";
-import NotFounded from "./../components/NotFounded";
-import StudentCourses from "./StudentCourses";
-import StudentProjectsPage from "./StudentProjectsPage";
-import StudentFriends from "./StudentFriends";
-import StudentFilesPage from "./StudentFilesPage";
-import StudentTablePage from "./StudentTablePage";
-import Library from "./../library/library";
+
+const StudentHomePage = lazy(() => import("./StudentHomePage"));
+const StudentSettingPage = lazy(() => import("./StudentSettingPage"));
+const NotFounded = lazy(() => import("./../components/NotFounded"));
+const StudentCourses = lazy(() => import("./StudentCourses"));
+const StudentProjectsPage = lazy(() => import("./StudentProjectsPage"));
+const StudentFriends = lazy(() => import("./StudentFriends"));
+const StudentFilesPage = lazy(() => import("./StudentFilesPage"));
+const StudentTablePage = lazy(() => import("./StudentTablePage"));
+const Library = lazy(() => import("./../library/library"));
 
 const Student = (props) => {
   useEffect(() => {
@@ -25,23 +26,36 @@ const Student = (props) => {
           <Head />
           <div className="pagewrapper display-flex gap-10">
             <div className="postContainer width-full">
-              <Routes>
-                <Route exact path="/home" element={<StudentHomePage />} />
-                <Route path="setting" element={<StudentSettingPage />}>
-                  <Route path="" exact />
-                  <Route path="student-setting-password" />
-                  <Route path="student-setting-email" />
-                  <Route path="*" render={() => <Navigate to="/" />} />
-                </Route>
-                <Route path="/projects" element={<StudentProjectsPage />} />
-                <Route path="/courses" element={<StudentCourses />} />
-                <Route path="/Friends" element={<StudentFriends />} />
-                <Route path="/Files" element={<StudentFilesPage />} />
-                <Route path="/table" element={<StudentTablePage />} />
-                <Route path="/library" element={<Library />} />
-                <Route exact path="/" element={<StudentHomePage />} />
-                <Route path="*" element={<NotFounded to="/NotFounded" />} />
-              </Routes>
+              <Suspense
+                fallback={
+                  <div class="dots-container ">
+                    <div class="dots">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                    <p>academic management system</p>
+                  </div>
+                }
+              >
+                <Routes>
+                  <Route exact path="/home" element={<StudentHomePage />} />
+                  <Route path="setting" element={<StudentSettingPage />}>
+                    <Route path="" exact />
+                    <Route path="student-setting-password" />
+                    <Route path="student-setting-email" />
+                    <Route path="*" render={() => <Navigate to="/" />} />
+                  </Route>
+                  <Route path="/projects" element={<StudentProjectsPage />} />
+                  <Route path="/courses" element={<StudentCourses />} />
+                  <Route path="/Friends" element={<StudentFriends />} />
+                  <Route path="/Files" element={<StudentFilesPage />} />
+                  <Route path="/table" element={<StudentTablePage />} />
+                  <Route path="/library" element={<Library />} />
+                  <Route exact path="/" element={<StudentHomePage />} />
+                  <Route path="*" element={<NotFounded to="/NotFounded" />} />
+                </Routes>
+              </Suspense>
             </div>
           </div>
         </div>
