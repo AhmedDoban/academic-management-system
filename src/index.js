315e75618ef580a13537c2a471de0b6c2c58f71d
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-02-02 17:43:26 +0200

    show student

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Admin />
+      <Student />
     </BrowserRouter>
   </React.StrictMode>
 );
