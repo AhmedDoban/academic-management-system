5b40679c643ce792788908da1edd980601d9c51b
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-02-24 02:29:51 +0200

    update library

---
+++ src/assets/js/Student/Library/Library.jsx
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import "./Library.css";
+import BookCard from "./bookCard";
+import RanDomQuote from "../Random Quote/RanDomQuote";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Footer from "../../components/Footer/Footer";
+import Head from "./../../components/Header/Head";
+import Dots from "../../components/Dots/Dots";
+import BookDetails from "./BookDetails";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import NotFounded from "../../components/NotFounded";
+
+function Library(props) {
+  let [search, setSearch] = useState("");
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState([]);
+  const [Empty, setEmpty] = useState(true);
+
+  useEffect(() => getData(), []);
+
+  const getData = (e) => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          (e ? e : "space") +
+          "&key=AIzaSyB1kPV9WkbAyngXClEvg3BBXN6ahnD-Nag" +
+          "&maxResults=40"
+      )
+      .then((response) => {
+        setItems(response.data.items);
+      });
+  };
+
+  let oneCall = (e) => {
+    if (Empty) {
+      getData();
+      setEmpty(false);
+    }
+  };
+  oneCall();
+
+  const searchBox = (e) => {
+    if (e.key === "Enter") {
+      if (search) {
+        getData(search);
+        setSearch("");
+      } else {
+        toast.error("You Must Enter Book Name", {
+          autoClose: 15000,
+          theme: "colored",
+        });
+      }
+    }
+  };
+  const searchButton = (e) => {
+    if (search) {
+      getData(search);
+      setSearch("");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Head />
+      <div className="Library">
+        <div className="head-Section">
+          <div className="container">
+            <h3>
+              Room Without <span>Books </span> Like Body Without
+              <span> Soul </span>
+            </h3>
+            <h5>Welcome to the world in it you will build your brain </h5>
+            <div className="search-box">
+              <button className="btn-search" onClick={searchButton}>
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+              <input
+                type="search"
+                placeholder="what are you looking for ? "
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyPress={searchBox}
+                className="input-search"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="content">
+          <h1 className="main-titel">
+            <div className="div-circle"></div>
+            <div className="div-circle"></div>
+            <span> Library Books</span>
+          </h1>
+          <Dots OtherStyle="top" />
+          <Dots OtherStyle="bottom" />
+
+          <Routes>
+            <Route
+              exact
+              path=""
+              element={<BookCard bookData={items} setItem={setItem} />}
+            />
+            <Route path=":id" element={<BookDetails item={item} />} />
+            <Route path="*" element={<NotFounded to="/NotFounded" />} />
+          </Routes>
+        </div>
+      </div>
+      <RanDomQuote />
+      <Footer />
+      <ToastContainer />
+    </React.Fragment>
+  );
+}
+export default Library;
---
+++ src/assets/js/Student/Library/bookCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import LodingFeachData from "../../components/LodingFeachData";
+
+function BookCard(props) {
+  return (
+    <React.Fragment>
+      <div className="books">
+        <div
+          className="container"
+          data-aos="fade-down"
+          data-aos-easing="ease-in-out"
+          data-aos-duration="1000"
+        >
+          {props.bookData.length > 0 ? (
+            props.bookData.map((p) => {
+              if (
+                p.volumeInfo.imageLinks &&
+                p.volumeInfo.imageLinks.smallThumbnail !== undefined &&
+                p.saleInfo.listPrice &&
+                p.saleInfo.listPrice.amount !== undefined
+              ) {
+                return (
+                  <>
+                    <Link
+                      to={p.volumeInfo.title}
+                      className="book"
+                      onClick={() => props.setItem(p)}
+                    >
+                      <div className="front">
+                        <div className="cover">
+                          <img
+                            src={
+                              p.volumeInfo.imageLinks &&
+                              p.volumeInfo.imageLinks.smallThumbnail
+                            }
+                            alt={p.volumeInfo.title}
+                          />
+                        </div>
+                      </div>
+                      <div className="left-side">
+                        <h2>
+                          <span>{p.volumeInfo.title}</span>
+                        </h2>
+                      </div>
+                    </Link>
+                  </>
+                );
+              }
+            })
+          ) : (
+            <LodingFeachData />
+          )}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+export default BookCard;
