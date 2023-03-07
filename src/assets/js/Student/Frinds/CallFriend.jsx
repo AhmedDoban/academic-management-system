a78e847e5bc6be3278347410f8112ccd979731f6
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-03-07 10:09:39 +0200

    update video call

--- src/assets/js/Student/Frinds/CallFriend.jsx
+++ src/assets/js/Student/Frinds/CallFriend.jsx
@@ -1,81 +1,208 @@
-import React, { useEffect, useState } from "react";
 import "./FriendsCall.css";
-import AgoraRTC from "agora-rtc-sdk-ng";
-import { VideoPlayer } from "./VideoPlayer";
-const AppId = "847c87d19cfd416597e7239fee2cf785";
-const Token =
-  "007eJxTYHDi/OOhImTPWyjz6eUNz7Tj+lPmp8pOfZjCM9n1x+s9VzkVGCxMzJMtzFMMLZPTUkwMzUwtzVPNjYwt01JTjZLTzC1MD3r+Sm4IZGTo3RfFwAiFID4Lg3NiTg4DAwAEAh8U";
-const Chanal = "Call";
+import React, { useEffect, useState } from "react";
+import {
+  AgoraVideoPlayer,
+  createClient,
+  createMicrophoneAndCameraTracks,
+  ClientConfig,
+} from "agora-rtc-react";
 
-const Client = AgoraRTC.createClient({
+const config: ClientConfig = {
   mode: "rtc",
   codec: "vp8",
-});
+};
 
-function CallFriend() {
+const appId = "f32a45e51edd4852b2d7dc8748f78b52";
+// const appId = "1ab3f923ed67455f81c8e4252d91fbc5";
+const token = null;
+
+const useClient = createClient(config);
+const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
+
+const VideoCall = (props) => {
+  const { setInCall, channelName } = props;
   const [users, setUsers] = useState([]);
-  const [localTracks, setLocalTracks] = useState([]);
-
-  const handleUserJoined = async (user, mediaType) => {
-    await Client.subscribe(user, mediaType);
-
-    if (mediaType === "video") {
-      setUsers((previousUsers) => [...previousUsers, user]);
-    }
-
-    if (mediaType === "audio") {
-      user.audioTrack.play();
-    }
-  };
-
-  const handleUserLeft = (user) => {
-    setUsers((previousUsers) =>
-      previousUsers.filter((u) => u.uid !== user.uid)
-    );
-  };
+  const [start, setStart] = useState(false);
+  const client = useClient();
+  const { ready, tracks } = useMicrophoneAndCameraTracks();
 
   useEffect(() => {
-    Client.on("user-published", handleUserJoined);
-    Client.on("user-left", handleUserLeft);
-
-    Client.join(AppId, Chanal, Token, null)
-      .then((uid) =>
-        Promise.all([AgoraRTC.createMicrophoneAndCameraTracks(), uid])
-      )
-      .then(([tracks, uid]) => {
-        const [audioTrack, videoTrack] = tracks;
-        setLocalTracks(tracks);
-
-        setUsers((previousUsers) => [
-          ...previousUsers,
-          {
-            uid,
-            videoTrack,
-            audioTrack,
-          },
-        ]);
-        Client.publish(tracks);
+    let init = async (name) => {
+      client.on("user-published", async (user, mediaType) => {
+        await client.subscribe(user, mediaType);
+        console.log("subscribe success");
+        if (mediaType === "video") {
+          setUsers((prevUsers) => {
+            return [...prevUsers, user];
+          });
+        }
+        if (mediaType === "audio") {
+          user.audioTrack?.play();
+        }
       });
 
-    return () => {
-      for (let localTrack of localTracks) {
-        localTrack.stop();
-        localTrack.close();
-      }
-      Client.off("user-published", handleUserJoined);
-      Client.off("user-left", handleUserLeft);
-      Client.unpublish(localTracks).then(() => Client.leave());
+      client.on("user-unpublished", (user, type) => {
+        console.log("unpublished", user, type);
+        if (type === "audio") {
+          user.audioTrack?.stop();
+        }
+        if (type === "video") {
+          setUsers((prevUsers) => {
+            return prevUsers.filter((User) => User.uid !== user.uid);
+          });
+        }
+      });
+
+      client.on("user-left", (user) => {
+        console.log("leaving", user);
+        setUsers((prevUsers) => {
+          return prevUsers.filter((User) => User.uid !== user.uid);
+        });
+      });
+
+      await client.join(appId, name, token, null);
+      if (tracks) await client.publish([tracks[0], tracks[1]]);
+      setStart(true);
     };
-  }, []);
+
+    if (ready && tracks) {
+      console.log("init ready");
+      init(channelName);
+    }
+  }, [channelName, client, ready, tracks]);
+
+  return (
+    <div className="App">
+      {start && tracks && <Videos users={users} tracks={tracks} />}
+      {ready && tracks && (
+        <Controls tracks={tracks} setStart={setStart} setInCall={setInCall} />
+      )}
+    </div>
+  );
+};
+
+const Videos = (props) => {
+  const { users, tracks } = props;
+
+  return (
+    <div>
+      <div id="videos">
+        <AgoraVideoPlayer className="vid active" videoTrack={tracks[1]} />
+        {users.length > 0 &&
+          users.map((user) => {
+            if (user.videoTrack) {
+              return (
+                <AgoraVideoPlayer
+                  className="vid"
+                  videoTrack={user.videoTrack}
+                  key={user.uid}
+                />
+              );
+            } else return null;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export const Controls = (props) => {
+  const client = useClient();
+  const { tracks, setStart, setInCall } = props;
+  const [trackState, setTrackState] = useState({ video: true, audio: true });
+
+  const mute = async (type) => {
+    console.log("client", client);
+    if (type === "audio") {
+      await tracks[0].setEnabled(!trackState.audio);
+      setTrackState((ps) => {
+        return { ...ps, audio: !ps.audio };
+      });
+    } else if (type === "video") {
+      await tracks[1].setEnabled(!trackState.video);
+      setTrackState((ps) => {
+        return { ...ps, video: !ps.video };
+      });
+    }
+  };
+
+  const leaveChannel = async () => {
+    await client.leave();
+    client.removeAllListeners();
+    tracks[0].close();
+    tracks[1].close();
+    setStart(false);
+    setInCall(false);
+  };
+
+  return (
+    <div className="controls">
+      <i
+        className={
+          trackState.audio
+            ? "fa-solid fa-microphone yes"
+            : "fa-solid fa-microphone-slash no"
+        }
+        onClick={() => mute("audio")}
+      ></i>
+      <i
+        className={
+          trackState.video
+            ? "fa-solid fa-video yes"
+            : "fa-solid fa-video-slash no"
+        }
+        onClick={() => mute("video")}
+      ></i>
+
+      {
+        <i
+          className="fa-solid fa-person-walking-dashed-line-arrow-right"
+          onClick={() => leaveChannel()}
+        ></i>
+      }
+    </div>
+  );
+};
+
+const ChannelForm = (props) => {
+  const { setInCall, setChannelName } = props;
 
   return (
     <React.Fragment>
-      <div className="video-container">
-        {users.map((user) => (
-          <VideoPlayer key={user.uid} user={user} />
-        ))}
+      <div className="card">
+        <p>Call Room</p>
+        <form className="join">
+          <input
+            type="text"
+            placeholder="Enter Channel Name"
+            onChange={(e) => setChannelName(e.target.value)}
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setInCall(true);
+            }}
+            className="submit-btn"
+          >
+            Join
+          </button>
+        </form>
       </div>
     </React.Fragment>
   );
-}
+};
+
+const CallFriend = () => {
+  const [inCall, setInCall] = useState(false);
+  const [channelName, setChannelName] = useState("");
+  return (
+    <div className="video-container">
+      {inCall ? (
+        <VideoCall setInCall={setInCall} channelName={channelName} />
+      ) : (
+        <ChannelForm setInCall={setInCall} setChannelName={setChannelName} />
+      )}
+    </div>
+  );
+};
+
 export default CallFriend;
