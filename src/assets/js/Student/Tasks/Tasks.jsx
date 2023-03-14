95e78bdafda7c1f05b0e99326363af9856f88bf6
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-03-14 20:08:42 +0200

    Update Tasks.jsx

--- src/assets/js/Student/Tasks/Tasks.jsx
+++ src/assets/js/Student/Tasks/Tasks.jsx
@@ -9,7 +9,7 @@ function Tasks(props) {
       <Head />
       <div className="tasks">
         <div className="container">
-          <h1 className="main-titel-2 ">Student Projects page</h1>
+          <h1 className="main-titel-2 ">Class Room</h1>
           <div className="cards-container">
             {/********************** Next Up **************************/}
             <div
@@ -22,26 +22,34 @@ function Tasks(props) {
                 <h5>Next Up</h5>
                 <p className="Next">5</p>
               </div>
-              <div className="card-body">
-                <p>Details</p>
-                <h5>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                  vitae ex delectus quisquam voluptatem rem mollitia maxime
-                  dignissimos reprehenderit. At illum saepe sit assumenda, fuga
-                  dicta rerum esse quia debitis?
-                </h5>
+              {/********************** Next Cards Wrapper **************************/}
+              <div
+                data-aos="fade-down"
+                data-aos-easing="ease-in-out"
+                data-aos-duration="1000"
+              >
+                <div className="card-body">
+                  <p>Details</p>
+                  <h5>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quis vitae ex delectus quisquam voluptatem rem mollitia
+                    maxime dignissimos reprehenderit. At illum saepe sit
+                    assumenda, fuga dicta rerum esse quia debitis?
+                  </h5>
+                </div>
+                <div className="footer-card Next">
+                  <p>
+                    <i className="fa-regular fa-clock"></i> Mar 26
+                  </p>
+                  <label htmlFor="FileUpload">
+                    <span>
+                      Upload File <i class="fa-solid fa-angles-up"></i>
+                    </span>
+                    <input type="file" name="" id="FileUpload" />
+                  </label>
+                </div>
               </div>
-              <div className="footer-card Next">
-                <p>
-                  <i className="fa-regular fa-clock"></i> Mar 26
-                </p>
-                <label htmlFor="FileUpload">
-                  <span>
-                    Upload File <i class="fa-solid fa-angles-up"></i>
-                  </span>
-                  <input type="file" name="" id="FileUpload" />
-                </label>
-              </div>
+              {/********************** Next Cards Wrapper End **************************/}
             </div>
             {/********************** In progress **************************/}
             <div
@@ -54,20 +62,28 @@ function Tasks(props) {
                 <h5>In progress</h5>
                 <p className="progress">5</p>
               </div>
-              <div className="card-body">
-                <p>Details</p>
-                <h5>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                  vitae ex delectus quisquam voluptatem rem mollitia maxime
-                  dignissimos reprehenderit. At illum saepe sit assumenda, fuga
-                  dicta rerum esse quia debitis?
-                </h5>
+              {/********************** In progress Cards Wrapper **************************/}
+              <div
+                data-aos="fade-down"
+                data-aos-easing="ease-in-out"
+                data-aos-duration="1000"
+              >
+                <div className="card-body">
+                  <p>Details</p>
+                  <h5>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quis vitae ex delectus quisquam voluptatem rem mollitia
+                    maxime dignissimos reprehenderit. At illum saepe sit
+                    assumenda, fuga dicta rerum esse quia debitis?
+                  </h5>
+                </div>
+                <div className="footer-card progress">
+                  <p>
+                    <i className="fa-regular fa-clock"></i> Mar 26
+                  </p>
+                </div>
               </div>
-              <div className="footer-card progress">
-                <p>
-                  <i className="fa-regular fa-clock"></i> Mar 26
-                </p>
-              </div>
+              {/********************** In progress Cards Wrapper End **************************/}
             </div>
             {/********************** Complete **************************/}
             <div
@@ -80,20 +96,28 @@ function Tasks(props) {
                 <h5>Complete</h5>
                 <p className="Complete">5</p>
               </div>
-              <div className="card-body">
-                <p>Details</p>
-                <h5>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-                  vitae ex delectus quisquam voluptatem rem mollitia maxime
-                  dignissimos reprehenderit. At illum saepe sit assumenda, fuga
-                  dicta rerum esse quia debitis?
-                </h5>
+              {/********************** Complete Cards Wrapper **************************/}
+              <div
+                data-aos="fade-down"
+                data-aos-easing="ease-in-out"
+                data-aos-duration="1000"
+              >
+                <div className="card-body">
+                  <p>Details</p>
+                  <h5>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quis vitae ex delectus quisquam voluptatem rem mollitia
+                    maxime dignissimos reprehenderit. At illum saepe sit
+                    assumenda, fuga dicta rerum esse quia debitis?
+                  </h5>
+                </div>
+                <div className="footer-card Complete">
+                  <p>
+                    <i className="fa-regular fa-clock"></i> Mar 26
+                  </p>
+                </div>
               </div>
-              <div className="footer-card Complete">
-                <p>
-                  <i className="fa-regular fa-clock"></i> Mar 26
-                </p>
-              </div>
+              {/********************** Complete Card Wrapper End **************************/}
             </div>
           </div>
         </div>
