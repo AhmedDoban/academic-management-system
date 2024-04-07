87ae0a8f842beae0b2b7094f0051d1f605960553
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2024-04-07 14:44:31 +0200

    update animation in header and update scroll to the top

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Guest from "./assets/js/Guest/Guest";
 import Auth from "./assets/js/Guest/Auth";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,7 @@ import {
 function App() {
   const Login = useSelector((User) => User.User.IsLogin);
   const Dispatch = useDispatch();
+  const BackToTopBtn = useRef();
 
   useEffect(() => {
     Dispatch(Login_Local());
@@ -18,11 +19,38 @@ function App() {
     } //eslint-disable-next-line
   }, [Login]);
 
-  if (!Login) {
-    return <Guest />;
-  } else {
-    return <Auth />;
-  }
+  const BackToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY >= 150) {
+      BackToTopBtn.current.style.display = "flex";
+    } else {
+      BackToTopBtn.current.style.display = "none";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <i
+        className="fas fa-arrow-up back-to-top"
+        style={{ display: "none" }}
+        onClick={() => BackToTop()}
+        ref={BackToTopBtn}
+      />
+      {!Login ? <Guest /> : <Auth />}
+    </React.Fragment>
+  );
 }
 
 export default App;
